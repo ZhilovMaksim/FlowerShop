9d5a8779b740ab2c8c5c292285f1ce51d7b6e110
Sanzhar Almatuly <sanzhar.almatuly@erg.kz>
2024-04-23 17:03:09 +0500

    написал круды для корзины

--- src/main/java/sample/flowershop/Shopping.java
+++ src/main/java/sample/flowershop/Shopping.java
@@ -28,31 +28,59 @@ class Product{
     public String getName() {
         return name;
     }
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
 }
 
-class ShoppingCart{
+class ShoppingCart {
     private List<Product> items;
-    public ShoppingCart(){
+
+    public ShoppingCart() {
         this.items = new ArrayList<>();
     }
-    public void addProduct(Product product){
+
+    public void addProduct(Product product) {
         items.add(product);
     }
-    public void removeProduct(int productId){
-        items.removeIf(product -> product.getId()==productId);
+
+    public void removeProduct(int productId) {
+        items.removeIf(product -> product.getId() == productId);
     }
-    public void displayCart(){
+
+    public void displayCart() {
         //MAX SDELAY HUYNYU
     }
 
-    private double calculateTotal(){
+    private double calculateTotal() {
         double total = 0;
-        for(Product item: items){
-            total+=item.getPrice();
+        for (Product item : items) {
+            total += item.getPrice();
         }
         return total;
     }
+
+    public void updateProduct(int productId, String newName, double newPrice, String newDescription) {
+        for (Product item : items) {
+            if (item.getId() == productId) {
+                item.setName(newName);
+                item.setPrice(newPrice);
+                item.setDescription(newDescription);
+                return;
+            }
+        }
+    }
 }
 public class Shopping {
+    public static void main(String[] args) {
 
+    }
 }
